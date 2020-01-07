0c86f8f0e02888300851f7b6c77986a1298d94da
johnschneider1 <johnlawschneider@gmail.com>
2020-01-07 12:23:37 -0600

    map work

--- client/src/components/gamePage/GameStyles.js
+++ client/src/components/gamePage/GameStyles.js
@@ -1,19 +1,46 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const GameGrid = styled.div`
-    margin-top: 100px;
-    width: 1200px;
-    display: grid;
-    grid-template-rows: 300px 300px 300px;
-    grid-template-columns: 300px 300px 300px 300px;
-    border: 2px solid black;
-    border-radius: 6px;
-    /* overflow: hidden; */
+  margin-top: 100px;
+  width: 1200px;
+  display: grid;
+  grid-template-rows: 300px 300px 300px;
+  grid-template-columns: 300px 300px 300px 300px;
+  border: 2px solid black;
+  border-radius: 6px;
+  /* overflow: hidden; */
 `;
 
 export const MapDiv = styled.div`
-    grid-column-start: 1;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 2;
+  grid-column-start: 1;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: grid;
+  grid-template-columns: repeat(20, 1fr);
+  grid-template-rows: repeat(20, 1fr);
+  grid-gap: 4px;
+`;
+
+export const ConnectImg = styled.img`
+  position: relative;
+  left: 50%;
+  top: 0;
+`;
+
+export const RoomDiv = styled.div`
+  grid-column-start: ${props => props.xCoord};
+  grid-row-start: ${props => props.yCoord};
+`;
+
+export const DirectionsDiv = styled.div`
+ display: flex;
+ flex-flow: column nowrap;
+ border-top: 2px solid black;
+ border-right 2px solid black;
+ grid-column-start: 1;
+ grid-column-end: 2;
+ grid-row-start: 2;
+ grid-row-end: 3;
+
 `;
--- client/src/components/gamePage/MapContainer.jsx
+++ client/src/components/gamePage/MapContainer.jsx
@@ -1,18 +1,47 @@
-import React from "react"
-import {useSelector, useDispatch} from "react-redux"
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 // import {action} from "../../actions"
-import { MapDiv } from './GameStyles'
+import { MapDiv, ConnectImg, RoomDiv } from "./GameStyles";
 
-const MapContainer = _ =>
-{
-    const dispatch = useDispatch()
-    const state = useSelector(state => state)
+const MapContainer = _ => {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
-    return (
-        <MapDiv>
+  return (
+    <MapDiv>
+      {state.exploredRooms.map(el => {
+        return (
+          <RoomDiv>
+            <img src="../../assets/room.jpg" alt="room" />
+            {el.n_to && (
+              <ConnectImg
+                src="../../assets/images/n_connector.jpg"
+                alt="connector"
+              />
+            )}
+            {el.s_to && (
+              <ConnectImg
+                src="../../assets/images/s_connector.jpg"
+                alt="connector"
+              />
+            )}
+            {el.e_to && (
+              <ConnectImg
+                src="../../assets/images/e_connector.jpg"
+                alt="connector"
+              />
+            )}
+            {el.w_to && (
+              <ConnectImg
+                src="../../assets/images/w_connector.jpg"
+                alt="connector"
+              />
+            )}
+          </RoomDiv>
+        );
+      })}
+    </MapDiv>
+  );
+};
 
-        </MapDiv>
-    )
-}
-
-export default MapContainer
+export default MapContainer;
