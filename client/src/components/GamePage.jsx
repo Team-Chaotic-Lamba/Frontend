a9f981c1f3a2858abc48ad42415ce5dfd09d1a19
Eli Sacks <bastlifa@gmail.com>
2020-01-06 23:21:46 -0500

    rough layout for app

---
+++ client/src/components/GamePage.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import {useSelector, useDispatch} from "react-redux"
+import {action} from "../../actions"
+
+const GamePage = _ =>
+{
+    const dispatch = useDispatch()
+    const state = useSelector(state => state)
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default GamePage
---
+++ client/src/components/header/HeaderStyles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const HeaderDiv = styled.div`
+    background-color: #82AA37;
+    border: 3px solid #3F5C09;
+    border-bottom-right-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border-top: none;
+    height: 50px;
+    font-size: 20px;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+    text-decoration: none;
+`;
+
