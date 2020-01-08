44c16089004072aafc18aa934446dcf68d5dc3d3
johnschneider1 <johnlawschneider@gmail.com>
2020-01-08 11:05:28 -0600

    styled about

--- client/src/components/aboutPage/AboutCard.jsx
+++ client/src/components/aboutPage/AboutCard.jsx
@@ -1,17 +1,27 @@
-import React from "react"
-import { CardDiv } from './AboutStyles'
+import React from "react";
+import { CardDiv, ImgDivGit, ImgDivLink, LinkBox } from "./AboutStyles";
 
-const AboutCard = (props) =>
-{
-    const { name, github, linkedIn, track } = props
-    return (
-        <CardDiv>
-            <h3 className='title is-3'>{name}</h3>
-            <h5 className='title is-5'>{track}</h5>
-            <a href={github}>Github</a>
-            <a href={linkedIn}>LinkedIn</a>
-        </CardDiv>
-    )
-}
+const AboutCard = props => {
+  const { name, github, linkedIn, track } = props;
 
-export default AboutCard
+  return (
+    <CardDiv>
+      <h3 className="title is-3">{name}</h3>
+      <h5 className="title is-5">{track}</h5>
+      <LinkBox>
+        <ImgDivGit>
+          <a href={github}>
+            <img src="https://i.imgur.com/AGL65ml.png" />
+          </a>
+        </ImgDivGit>
+        <ImgDivLink>
+          <a href={linkedIn}>
+            <img src="https://i.imgur.com/U7X2iES.png" />
+          </a>
+        </ImgDivLink>
+      </LinkBox>
+    </CardDiv>
+  );
+};
+
+export default AboutCard;
