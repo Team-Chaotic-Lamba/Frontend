e33262e594027fd158837411a5e24baa008a07a2
johnschneider1 <johnlawschneider@gmail.com>
2020-01-09 10:13:52 -0600

    need commit to merge

--- client/src/components/gamePage/ChatList.jsx
+++ client/src/components/gamePage/ChatList.jsx
@@ -1,33 +1,30 @@
-import React, {useEffect} from "react"
-import {useSelector, useDispatch} from "react-redux"
-import { subscribeToChats } from "../../actions"
-import { ChatP, ChatListDiv } from './GameStyles'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { subscribeToChats } from "../../actions";
+import { ChatP, ChatListDiv } from "./GameStyles";
 
-const ChatList = _ =>
-{
-    const dispatch = useDispatch()
-    const state = useSelector(state => state)
+const ChatList = _ => {
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
-    useEffect(() => {
-        const pusher = new Pusher("ed153050c9fa544407b6", {
-            cluster: "us2",
-            encrypted: true
-        });
-        const channel = pusher.subscribe("chat");
-        channel.bind("message", data => {
-            dispatch(subscribeToChats(data))
-        });
+  useEffect(() => {
+    const pusher = new Pusher("ed153050c9fa544407b6", {
+      cluster: "us2",
+      encrypted: true
+    });
+    const channel = pusher.subscribe("chat");
+    channel.bind("message", data => {
+      dispatch(subscribeToChats(data));
+    });
+  }, []);
 
-    }, [])
+  return (
+    <ChatListDiv>
+      {state.chatMessages.map((el, index) => {
+        return <ChatP key={index}>{el}</ChatP>;
+      })}
+    </ChatListDiv>
+  );
+};
 
-    return (
-        <ChatListDiv>
-            {state.chatMessages.map((el, index) => {
-            return (<ChatP key={index}>{el}</ChatP>)
-            })}
-
-        </ChatListDiv>
-    )
-}
-
-export default ChatList
+export default ChatList;
