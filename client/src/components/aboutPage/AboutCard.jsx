7c28a0352687a902b2ed5d06d7eb5f2173f69e22
Eli Sacks <bastlifa@gmail.com>
2020-01-09 13:20:55 -0500

    removed some unused imports, initialize on game page load

--- client/src/components/aboutPage/AboutCard.jsx
+++ client/src/components/aboutPage/AboutCard.jsx
@@ -11,12 +11,12 @@ const AboutCard = props => {
       <LinkBox>
         <ImgDivGit>
           <a href={github} target="_blank" rel="noopener noreferrer">
-            <img src="https://i.imgur.com/AGL65ml.png" />
+            <img src="https://i.imgur.com/AGL65ml.png" alt="icon" />
           </a>
         </ImgDivGit>
         <ImgDivLink>
           <a href={linkedIn} target="_blank" rel="noopener noreferrer">
-            <img src="https://i.imgur.com/U7X2iES.png" />
+            <img src="https://i.imgur.com/U7X2iES.png" alt="icon" />
           </a>
         </ImgDivLink>
       </LinkBox>
