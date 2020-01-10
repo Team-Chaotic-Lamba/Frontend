1a3da9d238eadc03a002d12d0caebfd199f51919
AndrewA0112 <andrew.mkal@outlook.com>
2020-01-09 18:42:05 -0600

    map hides room

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -151,6 +151,7 @@ export const reducer = (state = initialState, action) =>
             return {
                 ...state,
                 isLoading: false,
+                exploredRooms: action.payload.data.visited_room_ids,
                 currentRoom: {
                     title: action.payload.data.title,
                     description: action.payload.data.description,
